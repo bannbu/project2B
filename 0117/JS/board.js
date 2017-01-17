907d0df6da3ec0fedde1805d05d91b99322bd576
contena <nancontena@yahoo.co.jp>
2017-01-17 23:11:52 +0900

    tasukete

---
+++ 0117/JS/board.js
@@ -0,0 +1,31 @@
+$(function(){
+  getComment();
+  setInterval(getComment,3000);
+
+});
+
+function getComment(){
+var id =  $('#contentArea > .content:eq(0) > input:hidden').attr("value");
+// $(".showError > span").remove();
+// $('.showError > span').html("");
+$.ajax({
+  type:"POST",
+  url: "searchNewComment.php",
+  data: 'content_id='+ id,
+  // datatype: "json",
+  success: function(data){
+    // $('.showError').html(html);
+    // $("#contentArea").prepend(data);
+    console.log(data);
+  if(data !== null){
+    console.log(data);
+    console.log("success");
+    for(var i = 0; i < data.length;i++){
+      $("#contentArea").prepend(data[i].content_id);
+    }
+  }
+
+  }
+});
+// console.log(id);
+}
